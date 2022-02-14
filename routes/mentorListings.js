9dfc7c7fe1e278a7b339d34f7cebd435c798cbd7
abiramitoronto <nagaabirami.toronto@gmail.com>
2022-02-14 16:19:52 -0500

    Fix mentorlistings

--- routes/mentorListings.js
+++ routes/mentorListings.js
@@ -1,7 +1,7 @@
 /*
- * All routes for Widgets are defined here
- * Since this file is loaded in server.js into api/widgets,
- *   these routes are mounted onto /widgets
+ * All routes for menterListings are defined here
+ * Since this file is loaded in server.js into api/menterListings,
+ *   these routes are mounted onto /menterListings
  * See: https://expressjs.com/en/guide/using-middleware.html#middleware.router
  */
 
