31b262bb0dee062a859df266188a121784b4b52e
abiramitoronto <nagaabirami.toronto@gmail.com>
2022-02-14 19:12:11 -0500

    Delete name field in mentors db schema

--- db/schema/02_mentors.sql
+++ db/schema/02_mentors.sql
@@ -2,7 +2,6 @@ DROP TABLE IF EXISTS mentors CASCADE;
 CREATE TABLE mentors (
   id SERIAL PRIMARY KEY NOT NULL,
   user_id INTEGER REFERENCES users(id),
-  name VARCHAR(255) NOT NULL,
   description TEXT,
   speciality TEXT,
   location VARCHAR(255) NOT NULL,
