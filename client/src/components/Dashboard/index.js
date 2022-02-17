8cebe8ed6a6a1c2ece5d2ef4778fbad07a02ecc4
Anknv <itsannakonovalova@gmail.com>
2022-02-17 21:58:08 +0000

    add a calendar component

--- client/src/components/Dashboard/index.js
+++ client/src/components/Dashboard/index.js
@@ -1,12 +1,14 @@
+import { Calendar } from "./Calendar"
 import { Goals } from "./Goals"
 import { MentorCard } from "./MentorCard"
 import './styles.css'
 
 export const Dashboard = function(props) {
   return <div>
+    <div className="dashboard-month-picker">February 2022</div>
     <div className="dashboard-top-row">
       <MentorCard monthId='1' />
-      <div>Calendar...</div>
+      <Calendar month={1} year={2022} />
     </div>
     <Goals monthId='1' />
   </div>
