ee5347dbb514e38b67ced8e246894b6f8efd3a91
Zeynep <zeynepkaya_26@hotmail.com>
2022-02-18 20:10:40 -0500

    Add more css to find mentor page

--- client/src/components/Mentors/Mentors.js
+++ client/src/components/Mentors/Mentors.js
@@ -2,35 +2,50 @@ import React from "react";
 import './Mentor.css';
 import { Link } from 'react-router-dom';
 import getSpots from "../../hooks/getSpots";
+import { BsChatDotsFill } from 'react-icons/bs';
+import { MdCall, MdTaskAlt } from 'react-icons/md';
 
 
 export default function Mentors(props) {
 
-
   const state = props.data;
-  console.log("Vacant Spots",state.vacantspots);
+  console.log("Vacant Spots", state.vacantspots);
 
-
-
-  const mentorListings = state.mentorlist.mentorList && state.mentorlist.mentorList.map(user =>
-    {
-     return <article className="mentor">
+  const mentorListings = state.mentorlist.mentorList && state.mentorlist.mentorList.map(user => {
+    return <article className="mentor">
       <header className="mentor--header">
         <img className="mentor--avatar" src={user.image_url} />
-        <div className="mentor--desc">{user.description}</div>
+        <div className="mentor--desc">
+          <h2 className="h2">About Me</h2>
+          <div className="mentor--desc">{user.description}</div>
+        </div>
       </header>
       <div className="mentor--body">
         <p>Name       : {user.name}</p>
-        <p>Email      : {user.email}</p>
         <p>Speciality : {user.speciality}</p>
         <p>Language   : {user.language}</p>
         <p>Location   : {user.location}</p>
-        <p>Spots Available : {getSpots(user.user_id,state.vacantspots.available_spots)}</p>
+        <p>Spots Available : {getSpots(user.user_id, state.vacantspots.available_spots)}</p>
 
       </div>
 
       <div className="book--body">
-        <h3>$100/ month</h3> <br />
+        <div className="footer">
+          <div className="fa-icons">
+            <div className="chat-icon">Chat
+              <BsChatDotsFill />
+            </div>
+            <div className="call-icon">2 Calls
+              <MdCall />
+            </div>
+            <div className="tasks-icon">Tasks
+              <MdTaskAlt />
+            </div>
+          </div>
+          <div className="monthly-pay">
+            <h3>$100/ month</h3> <br />
+          </div>
+        </div>
         {props.user &&
           <Link className='book-links' to={{
             pathname: "bookmentor",
@@ -43,7 +58,7 @@ export default function Mentors(props) {
 
 
     </article>
-    });
+  });
 
   return (
     <section className="tweets">
