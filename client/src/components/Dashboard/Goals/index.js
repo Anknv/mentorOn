0e3ce7de9c5504865bc28505a1c943cdafdc6bd4
Zeynep <zeynepkaya_26@hotmail.com>
2022-02-20 19:07:51 -0500

    Add css to goals

--- client/src/components/Dashboard/Goals/index.js
+++ client/src/components/Dashboard/Goals/index.js
@@ -4,7 +4,7 @@ import { CircularProgressbar } from 'react-circular-progressbar';
 import 'react-circular-progressbar/dist/styles.css';
 import './styles.css'
 
-export const Goals = function(props) {
+export const Goals = function (props) {
 
   const [goals, setGoals] = useState([]);
 
@@ -19,10 +19,10 @@ export const Goals = function(props) {
         user_id: props.userId,
       }
     })
-    .then((response) => {
-      console.log('goals', {response});
-      setGoals(response.data);
-    })
+      .then((response) => {
+        console.log('goals', { response });
+        setGoals(response.data);
+      })
   }
 
   function markGoalDone(goalId, isDone) {
@@ -54,32 +54,34 @@ export const Goals = function(props) {
   }, 0) / goals.length * 100) || 0;
 
   return (
-  <div className="goals">
-    <div className="goals--list-container">
-      <h3 className="goals--title-header">Goals</h3>
-      <ul className="goals--list">
-        {goals.map(goal => (
-          <li className="goals--list-item" key={goal.id}>
-            {props.userId ? '' : <input type='checkbox' onChange={(event) => markGoalDone(goal.id, event.target.checked)} checked={goal.is_done} />}
-            {goal.description}
-          </li>
-        ))}
-      </ul>
-      {goals.length < 5 && props.userId ? <input
-        className="goals--new-goal"
-        type='text'
-        onKeyDown={event => {
-          if (event.key === 'Enter') {
-            addNewGoal(event.target.value);
-            event.target.value = '';
-          }
-        }}
-        placeholder="Type a new goal..."
-      /> : ''}
+    <div className="goals">
+      <div className="goals--list-container">
+        <h3 className="goals--title-header">Goals</h3>
+        <ul className="goals--list">
+          {goals.map(goal => (
+            <li className="goals--list-item" key={goal.id}>
+              {props.userId ? '' : <input type='checkbox' onChange={(event) => markGoalDone(goal.id, event.target.checked)} checked={goal.is_done} />}
+              <div className=" goal-desc">
+                {goal.description}
+              </div>
+            </li>
+          ))}
+        </ul>
+        {goals.length < 5 && props.userId ? <input
+          className="goals--new-goal"
+          type='text'
+          onKeyDown={event => {
+            if (event.key === 'Enter') {
+              addNewGoal(event.target.value);
+              event.target.value = '';
+            }
+          }}
+          placeholder="Type a new goal..."
+        /> : ''}
+      </div>
+      <div className='goals--progress-circle'>
+        <CircularProgressbar value={percentage} text={`${percentage}%`} />
+      </div>
     </div>
-    <div className='goals--progress-circle'>
-      <CircularProgressbar value={percentage} text={`${percentage}%`} />
-    </div>
-  </div>
   )
 }
