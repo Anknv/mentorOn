31f4e9a09b1c91e1a7931d3833efbd8655ac1fd2
Anknv <itsannakonovalova@gmail.com>
2022-02-22 16:35:09 +0000

    db

--- db/seeds/04_sessions.sql
+++ db/seeds/04_sessions.sql
@@ -1,3 +1,5 @@
 INSERT INTO sessions (user_id,mentor_id, month_id) VALUES (5,2,1);
-INSERT INTO sessions (user_id,mentor_id, month_id) VALUES (6,2,2);
+INSERT INTO sessions (user_id,mentor_id, month_id) VALUES (6,2,1);
+INSERT INTO sessions (user_id,mentor_id, month_id) VALUES (7,2,1);
+INSERT INTO sessions (user_id,mentor_id, month_id) VALUES (8,2,2);
 INSERT INTO sessions (user_id,mentor_id, month_id) VALUES (6,3,1);
