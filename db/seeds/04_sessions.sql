a906b091c51a7d5f129908403b46c67c831a7293
abiramitoronto <nagaabirami.toronto@gmail.com>
2022-02-22 16:44:03 -0500

    rearrange the seeds entries

--- db/seeds/04_sessions.sql
+++ db/seeds/04_sessions.sql
@@ -1,5 +1,5 @@
-INSERT INTO sessions (user_id,mentor_id, month_id) VALUES (5,2,1);
-INSERT INTO sessions (user_id,mentor_id, month_id) VALUES (6,2,1);
-INSERT INTO sessions (user_id,mentor_id, month_id) VALUES (7,2,1);
-INSERT INTO sessions (user_id,mentor_id, month_id) VALUES (8,2,2);
-INSERT INTO sessions (user_id,mentor_id, month_id) VALUES (6,3,1);
+INSERT INTO sessions (user_id,mentor_id, month_id) VALUES (11,2,1);
+INSERT INTO sessions (user_id,mentor_id, month_id) VALUES (12,2,1);
+INSERT INTO sessions (user_id,mentor_id, month_id) VALUES (13,2,1);
+INSERT INTO sessions (user_id,mentor_id, month_id) VALUES (14,1,2);
+INSERT INTO sessions (user_id,mentor_id, month_id) VALUES (12,3,2);
