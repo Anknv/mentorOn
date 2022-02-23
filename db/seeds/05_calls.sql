43ca5eb1fc61870a4a8e99d29ab1ec0b4e27e5c3
Anknv <itsannakonovalova@gmail.com>
2022-02-23 00:00:16 +0000

    fix seeds/calls

--- db/seeds/05_calls.sql
+++ db/seeds/05_calls.sql
@@ -1,2 +1,4 @@
 INSERT INTO calls (session_id, day_of_month) VALUES (1, 5);
+INSERT INTO calls (session_id, day_of_month) VALUES (2, 11);
 INSERT INTO calls (session_id, day_of_month) VALUES (1, 20);
+INSERT INTO calls (session_id, day_of_month) VALUES (2, 22);
